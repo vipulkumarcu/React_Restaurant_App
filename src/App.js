fdd464be7be524d0d5beb1c5b8b69b67bae27e4c
vipulkumar.gts@gmail.com <vipulkumar.gts@gmail.com>
2024-06-04 17:28:17 +0530

    sharpener - summary added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./Components/Layout/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 
 function App ()
 {
   return (
     <>
       <Header />
+      <MealsSummary />
     </>
   );
 }
